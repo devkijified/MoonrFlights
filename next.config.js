3958cccc28fd6ea19d44fc0acd53cd0b1690d8c0
Boluwatife Olaniyan <42306524+devkijified@users.noreply.github.com>
2026-06-14 19:36:47 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ['gkvxnxpdwrwaebmpcxej.supabase.co'],
   },
+  output: 'standalone',
+  distDir: '.next',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
