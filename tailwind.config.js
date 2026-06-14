09100cd00f8a088c0f7244f09ff104777a9b0f0c
Boluwatife Olaniyan <42306524+devkijified@users.noreply.github.com>
2026-06-14 20:02:55 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fade-in': 'fadeIn 0.2s ease-in-out',
+        'zoom-in': 'zoomIn 0.2s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        zoomIn: {
+          '0%': { transform: 'scale(0.95)', opacity: '0' },
+          '100%': { transform: 'scale(1)', opacity: '1' },
+        },
+      },
+    },
   },
   plugins: [],
 }
